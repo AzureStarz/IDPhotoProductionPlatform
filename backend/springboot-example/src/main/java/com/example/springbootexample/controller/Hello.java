00e8ad55c185ceba42ca80fcad75fcdeffea7c38
yyy1000 <69566909+yyy1000@users.noreply.github.com>
2022-04-17 12:25:19 +0800

    feat():add docs content

--- backend/springboot-example/src/main/java/com/example/springbootexample/controller/Hello.java
+++ backend/springboot-example/src/main/java/com/example/springbootexample/controller/Hello.java
@@ -19,25 +19,7 @@ public class Hello {
         String fileName = params.get("fileName");
         String url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg";
         try {
-            // 本地文件路径
-            //String filePath = "D:\\test\\2.jpg";
-            //获取项目classes/static的地址
-            // System.out.println(imgStr+ fileName);
-            // String staticPath = ClassUtils.getDefaultClassLoader().getResource("static").getPath();
-            // String fileName = file.getOriginalFilename();  //获取文件名
-            // imgStr = imgStr.replace("^data:image/w+;base64,", "");
-            /*
-            // 图片存储目录及图片名称
-            String url_path = "images" + File.separator + fileName;
-            String url_path_new_photo = "images" + File.separator+ "modify-" + fileName;
-            //图片保存路径
-            String savePath = staticPath + File.separator + url_path;
-            String savePathNew = staticPath + File.separator + url_path_new_photo;
-            // 访问路径=静态资源路径+文件目录路径
-            // String visitPath ="static/" + url_path_new_photo;
-            */
-            //byte[] imgData = FileUtil.readFileByBytes(filePath);
-            //String imgStr = Base64Util.encode(imgData);
+
             String imgParam = URLEncoder.encode(imgStr, "UTF-8");
 
             String param = "image=" + imgParam;
