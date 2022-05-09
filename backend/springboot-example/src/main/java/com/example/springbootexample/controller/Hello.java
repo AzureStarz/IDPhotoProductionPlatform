285f4ba24f8f1d4e4b5de006a19fc2483e78ed6d
yyy1000 <69566909+yyy1000@users.noreply.github.com>
2022-05-09 22:30:48 +0800

    feat(): add beautify

--- backend/springboot-example/src/main/java/com/example/springbootexample/controller/Hello.java
+++ backend/springboot-example/src/main/java/com/example/springbootexample/controller/Hello.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
 import com.example.springbootexample.Utils.*;
-import java.io.File;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -67,6 +70,71 @@ public class Hello {
         //return "hello world <img src=\"images/test.jpg\">";
     }
 
+    @PostMapping("/api/beautify")
+    public String beatuify(@RequestBody Map<String, String> params){
+        String imgStr = params.get("imgStr");
+        String fileName = params.get("fileName");
+        Integer userId = Integer.parseInt(params.get("userId"));
+        String imagePath = "lib/time.jpg";
+
+        OutputStreamWriter out = null;
+        BufferedReader in = null;
+        StringBuilder result = new StringBuilder();
+        HttpURLConnection conn = null;
+        try{
+            URL url = new URL("https://openapi.mtlab.meitu.com/v3/makeup?api_key=e39297a28983493185cb529b2fdf70e0&api_secret=95a47f533cce48beb4e253d2028da4b2");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setConnectTimeout(30000);
+            conn.setReadTimeout(10000);
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            out = new OutputStreamWriter(conn.getOutputStream());
+            String jsonStr = "{\"parameter\":{\"beautyAlpha\":70},\"extra\":{},\"media_info_list\":[{\"media_data\":\""+ imgStr +"\",\"media_profiles\":{\"media_data_type\":\"jpg\"}}]}";
+            out.write(jsonStr);
+            out.flush();
+            out.close();
+
+            if (200 == conn.getResponseCode()){
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            }else{
+                System.out.println("ResponseCode is an error code:" + conn.getResponseCode());
+                in = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+            }
+            String line;
+            while ((line = in.readLine()) != null){
+                result.append(line);
+                //System.out.println(line);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(out != null){
+                    out.close();
+                }
+                if(in != null){
+                    in.close();
+                }
+            }catch (IOException ioe){
+                ioe.printStackTrace();
+            }
+        }
+        String newFilename = "beautify-"+ fileName;
+        String newPath = FileUtil.GetFrontEndPath(newFilename);
+        String base64NewImg=result.toString();
+        Renew.GenerateImage(base64NewImg, newPath);
+        Photo newPhoto = new Photo();
+        newPhoto.setUserId(userId);
+        newPhoto.setPhotoPath(newPath);
+        newPhoto.setPhotoName(newFilename);
+        photoDao.savePhoto(newPhoto);
+        return newFilename;
+    }
     @PostMapping("/api/getPhotos")
     public List<Photo> getPhotos(@Param("userId") Integer userId){
         return photoDao.getPhotosWithUserId(userId);
